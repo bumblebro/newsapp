d791e4e0d3b69aa03f777a843b5edda25292fe1f
Shreyas M S <shreyasmadappady20@gmail.com>
2024-12-25 22:05:46 +0530

    c

--- src/services/Guardianapi.js
+++ src/services/Guardianapi.js
@@ -1,28 +1,3 @@
-// import axios from "axios";
-
-// async function Guardianapi(input) {
-//   const result = await axios(
-//     `https://content.guardianapis.com/search?q=${input}&api-key=243914e8-8901-4435-8d44-80573fdbe98d&page-size=20`
-//   );
-//   const data = await result.data;
-//   return {
-//     totalarticles: data.response.total,
-//     articles: data.response.results,
-//   };
-// }
-
-// export async function Guardianapibycategory(category) {
-//   let url = `https://content.guardianapis.com/search?section=${category}&api-key=243914e8-8901-4435-8d44-80573fdbe98d&page-size=20`;
-//   const result = await axios(url);
-//   const data = await result.data;
-//   return {
-//     totalarticles: data.response.total,
-//     articles: data.response.results,
-//   };
-// }
-
-// export default Guardianapi;
-
 import axios from "axios";
 
 async function Guardianapi(input) {
--- src/services/Newsapi.js
+++ src/services/Newsapi.js
@@ -1,18 +1,3 @@
-// import axios from "axios";
-
-// async function Newsapi(input) {
-//   const result = await axios(
-//     `https://newsapi.org/v2/everything?q=${input}&pageSize=20&sortBy=publishedAt&apiKey=383085d771914007a402ed8e95ffe29c`
-//   );
-//   const data = await result.data;
-//   return {
-//     totalarticles: data.totalResults,
-//     articles: data.articles,
-//   };
-// }
-
-// export default Newsapi;
-
 import axios from "axios";
 
 async function Newsapi(input) {
